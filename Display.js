122f356717d56a8ad5e7af804e444b1480c7189a
Diana Campos <89281129+Diana-Camz@users.noreply.github.com>
2023-08-10 14:44:30 -0600

    add the functionality with Javascript

---
+++ Display.js
@@ -0,0 +1,57 @@
+class Display {
+  constructor(displayPreValue, displayCurrValue){
+    this.displayCurrValue = displayCurrValue;
+    this.displayPreValue = displayPreValue;
+    this.calculator = new Calculator();
+    this.operatorType = undefined;
+    this.preValue = "";
+    this.currValue = "";
+    this.signs = {
+      add: "+",
+      substract: "-",
+      divide: "/",
+      multiply:"*",
+    }
+  }
+
+  addNumber(number) {
+    if(number === "." && this.currValue.includes(".")) return
+    this.currValue = this.currValue.toString() + number.toString();
+    this.printValues();
+  }
+
+  eraseNumber(){
+    this.currValue = this.currValue.toString().slice(0,-1);
+    this.printValues();
+  }
+  
+  clearAll(){
+    this.operatorType = undefined;
+    this.preValue = "";
+    this.currValue = "";
+    this.printValues();
+  }
+
+  doCalculation(type){
+    
+    this.operatorType !== "equal" && this.calculate();
+    this.operatorType = type;
+    this.preValue = this.currValue || this.preValue;
+    this.currValue = "";
+    this.printValues();
+  }
+
+  printValues(){
+    this.displayCurrValue.textContent = this.currValue;
+    this.displayPreValue.textContent = `${this.preValue} ${this.signs[this.operatorType] || ""} `;
+  }
+
+  calculate(){
+    const preValue = parseFloat(this.preValue);
+    const currValue = parseFloat(this.currValue);
+
+
+    if(isNaN(currValue) || isNaN(preValue)) return
+    this.currValue = this.calculator[this.operatorType](preValue, currValue);
+  }
+}
--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-document.write("hola")
+const preValue = document.getElementById("preValue");
+const currValue = document.getElementById("currValue");
+const buttoNumber = document.querySelectorAll("#number"); 
+const buttonSign = document.querySelectorAll("#sign"); 
+const erase = document.getElementsByClassName(".erase");
+const clear = document.getElementsByClassName(".clear");
+const display = new Display(preValue, currValue);
+
+
+buttoNumber.forEach( button => {
+  button.addEventListener("click", () => display.addNumber(button.innerHTML));
+});
+
+buttonSign.forEach( button => {
+  button.addEventListener("click", () => display.doCalculation(button.value));
+});
+
